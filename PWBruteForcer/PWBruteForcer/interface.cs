9e5c076253e18db888928aa3b957943e0b00be18
BogdanBargaoanu <bn19bba@yahoo.com>
2024-06-13 21:41:25 +0300

    ip regex and username check

--- PWBruteForcer/PWBruteForcer/interface.cs
+++ PWBruteForcer/PWBruteForcer/interface.cs
@@ -25,14 +25,29 @@ namespace PWBruteForcer
 
         private void attemptLoginBtn_Click(object sender, EventArgs e)
         {
-            string command = $@"cmd.exe /c ""{batchFilePath} {textBoxIP.Text} {passwordsListPath} {textBoxUsername.Text}""";
+            if (textBoxUsername.Text.Trim().Length <= 0)
+            {
+                MessageBox.Show("Please enter a username.", "Warning.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxUsername.Focus();
+                return;
+            }
+
+            // Regex for IPv4: Matches exactly four octets (0-255), separated by dots. Anchored to start (^) and end ($) of string.
+            string ipPattern = @"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+            if (!System.Text.RegularExpressions.Regex.IsMatch(textBoxIP.Text.Trim(), ipPattern))
+            {
+                MessageBox.Show("Please enter a valid IP address.", "Warning.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBoxIP.Focus();
+                return;
+            }
+            string command = $@"cmd.exe /c ""{batchFilePath} {textBoxIP.Text.Trim()} {passwordsListPath} {textBoxUsername.Text.Trim()}""";
             System.Diagnostics.Process.Start("cmd.exe", command);
         }
 
         private void Interface_Load(object sender, EventArgs e)
         {
             DirectoryInfo directoryInfo = Directory.GetParent(executablePath); // Up one level to Debug
-            for (int i = 0; i < 4; i++) // Repeat four more times to go up five levels in total
+            for (int i = 0; i < 4; i++) // Repeat three more times to go up five levels in total
             {
                 directoryInfo = directoryInfo?.Parent;
             }
